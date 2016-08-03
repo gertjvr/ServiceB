a9f236a8f1360e880b425724a5e34286ffbad1cd
Gert Jansen van Rensburg <gertjvr@gmail.com>
2016-08-03 23:07:50 +1000

    Added GitVersion.yml

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyDescription("ServiceB")]
 [assembly: AssemblyCompany("Solutions")]
 [assembly: AssemblyProduct("ServiceB")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
 [assembly: AssemblyCopyright("Copyright (c) 2016")]
 
